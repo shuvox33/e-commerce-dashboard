3b2a3eca94165461a9f0eada600d4a343770cd83
shuvox33 <shuvox33@gmail.com>
2024-10-27 22:04:10 +0600

    added customers pages

--- src/pages/Customers.tsx
+++ src/pages/Customers.tsx
@@ -1,10 +1,104 @@
+import { ReactElement, useCallback, useState } from "react";
 import AdminSideBar from "../components/AdminSideBar";
+import { Column } from "react-table";
+import TableHOC from "../components/TableHOC";
+import { FaTrash } from "react-icons/fa";
+
+
+interface DataType {
+    avatar: ReactElement;
+    name: string;
+    email: string;
+    gender: string;
+    role: string;
+    action: ReactElement;
+}
+
+
+const column: Column<DataType>[] = [
+    {
+        Header: 'Avatar',
+        accessor: 'avatar',
+    },
+    {
+        Header: 'Name',
+        accessor: 'name',
+    },
+    {
+        Header: 'Emain',
+        accessor: 'email',
+    },
+    {
+        Header: 'Gender',
+        accessor: 'gender',
+    },
+    {
+        Header: 'Role',
+        accessor: 'role',
+    },
+    {
+        Header: 'Action',
+        accessor: 'action',
+    },
+]
+
+const img = "https://randomuser.me/api/portraits/women/54.jpg";
+const img2 = "https://randomuser.me/api/portraits/women/50.jpg";
+
+const arr: DataType[] = [
+    {
+        avatar: (
+            <img
+                style={{
+                    borderRadius: "50%",
+                }}
+                src={img}
+                alt="Shoes"
+            />
+        ),
+        name: "Emily Palmer",
+        email: "emily.palmer@example.com",
+        gender: "female",
+        role: "user",
+        action: (
+            <button>
+                <FaTrash />
+            </button>
+        ),
+    },
+
+    {
+        avatar: (
+            <img
+                style={{
+                    borderRadius: "50%",
+                }}
+                src={img2}
+                alt="Shoes"
+            />
+        ),
+        name: "May Scoot",
+        email: "aunt.may@example.com",
+        gender: "female",
+        role: "user",
+        action: (
+            <button>
+                <FaTrash />
+            </button>
+        ),
+    },
+];
 
 const Customers = () => {
+
+    const [data] = useState<DataType[]>(arr);
+
+    const Table = useCallback(TableHOC<DataType>(column, data, "dashboard-product-box", "Customers", true), []);
+
     return (
         <div className="admin-container">
-            <AdminSideBar/>
-            <main>main</main>
+            <AdminSideBar />
+            <main>{Table()}</main>
         </div>
     );
 };
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -18,6 +18,8 @@ const img =
     "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXN8ZW58MHx8MHx8&w=1000&q=804";
 
 const img2 = "https://m.media-amazon.com/images/I/514T0SvwkHL._SL1500_.jpg";
+
+
 const column: Column<DataType>[] = [
     {
         Header: 'Photo',
