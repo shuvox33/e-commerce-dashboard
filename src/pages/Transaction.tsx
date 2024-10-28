876771acbc480f5ce69072ac29b7297556e0daa7
shuvox33 <shuvox33@gmail.com>
2024-10-28 22:15:09 +0600

    started transaction page

--- src/pages/Transaction.tsx
+++ src/pages/Transaction.tsx
@@ -1,9 +1,48 @@
+import { Column } from "react-table";
 import AdminSideBar from "../components/AdminSideBar";
+import { ReactElement } from "react";
+
+interface DataType {
+    avatar: ReactElement;
+    name: string;
+    email: string;
+    gender: string;
+    role: string;
+    action: ReactElement;
+}
+
+
+const column: Column<DataType>[] = [
+    {
+        Header: 'Avatar',
+        accessor: 'avatar',
+    },
+    {
+        Header: 'Name',
+        accessor: 'name',
+    },
+    {
+        Header: 'Emain',
+        accessor: 'email',
+    },
+    {
+        Header: 'Gender',
+        accessor: 'gender',
+    },
+    {
+        Header: 'Role',
+        accessor: 'role',
+    },
+    {
+        Header: 'Action',
+        accessor: 'action',
+    },
+]
 
 const Transaction = () => {
     return (
         <div className="admin-container">
-            <AdminSideBar/>
+            <AdminSideBar />
             <main>main</main>
         </div>
     );
