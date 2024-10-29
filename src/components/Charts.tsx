63ce88e3637085d6ead9b74c2857104637f0627b
shuvox33 <shuvox33@gmail.com>
2024-10-29 15:43:14 +0600

    added line chart page

--- src/components/Charts.tsx
+++ src/components/Charts.tsx
@@ -9,8 +9,11 @@ import {
   ChartData,
   ChartOptions,
   ArcElement,
+  LineElement,
+  PointElement,
+  Filler,
 } from "chart.js";
-import { Bar, Doughnut, Pie } from "react-chartjs-2";
+import { Bar, Doughnut, Line, Pie } from "react-chartjs-2";
 
 ChartJS.register(
   CategoryScale,
@@ -19,7 +22,10 @@ ChartJS.register(
   Title,
   Tooltip,
   Legend,
-  ArcElement
+  ArcElement,
+  LineElement,
+  PointElement,
+  Filler
 );
 
 const months = ["January", "February", "March", "April", "May", "June", "July"];
@@ -174,3 +180,60 @@ export const PieChart = ({
 
   return <Pie data={pieChartData} options={pieChartOptions} />;
 };
+
+interface LineChartProps {
+    data: number[];
+    label: string;
+    backgroundColor: string;
+    borderColor: string;
+    labels?: string[];
+  }
+  
+  export const LineChart = ({
+    data,
+    label,
+    backgroundColor,
+    borderColor,
+    labels = months,
+  }: LineChartProps) => {
+    const options: ChartOptions<"line"> = {
+      responsive: true,
+      plugins: {
+        legend: {
+          display: false,
+        },
+        title: {
+          display: false,
+        },
+      },
+  
+      scales: {
+        y: {
+          beginAtZero: true,
+          grid: {
+            display: false,
+          },
+        },
+        x: {
+          grid: {
+            display: false,
+          },
+        },
+      },
+    };
+  
+    const lineChartData: ChartData<"line", number[], string> = {
+      labels,
+      datasets: [
+        {
+          fill: true,
+          label,
+          data,
+          backgroundColor,
+          borderColor,
+        },
+      ],
+    };
+  
+    return <Line options={options} data={lineChartData} />;
+  };
