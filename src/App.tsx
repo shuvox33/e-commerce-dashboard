68e69e2a8e5aefc9ec26761095f9862983f05a9a
shuvox33 <shuvox33@gmail.com>
2024-10-31 10:40:09 +0600

    fix the app page

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,16 @@ import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Loader from "./components/Loader";
 
-
 const Dashboard = lazy(() => import("./pages/Dashboard"));
 const Customers = lazy(() => import("./pages/Customers"));
 const Products = lazy(() => import("./pages/Products"));
 const Transaction = lazy(() => import("./pages/Transaction"));
 const NewProduct = lazy(() => import("./pages/management/NewProduct"));
-
 const BarCharts = lazy(() => import("./pages/charts/BarChart"));
 const LineCharts = lazy(() => import("./pages/charts/LineCharts"));
 const PieCharts = lazy(() => import("./pages/charts/PieCharts"));
-
-
 const Coupon = lazy(() => import("./pages/apps/Coupon"));
 const StopWatch = lazy(() => import("./pages/apps/StopWatch"));
-
 const ProductManagement = lazy(
   () => import("./pages/management/ProductManagement")
 );
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            homw
+        </div>
+    );
+};
+
+export default Home;
